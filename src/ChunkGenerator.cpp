e4b33b22043458d88b023e991de7839fc0d77ded
Sam van Kampen <sam@tehsvk.net>
2016-07-05 15:44:08 +0200

    [chunkgen] Fix spaces/tabs, edit include locations and add logging

--- src/ChunkGenerator.cpp
+++ src/ChunkGenerator.cpp
@@ -1,43 +1,48 @@
-#include "ChunkGenerator.hpp"
-
-#include "Square.hpp"
+#include <nm/ChunkGenerator.hpp>
+#include <nm/Square.hpp>
 #include <random>
 #include <iostream>
 #include <cmath>
 
+#include <boost/log/trivial.hpp>
+
 namespace nm
 {
-    ChunkGenerator::ChunkGenerator(double mean_density, double variation)
-        : mean_density(mean_density), variation(variation) {}
+	ChunkGenerator::ChunkGenerator(double mean_density, double variation) : mean_density(mean_density), variation(variation)
+	{
+	};
 
-    Chunk ChunkGenerator::generate() {
-        Chunk chunk;
+	Chunk ChunkGenerator::generate()
+	{
+		Chunk chunk;
 
-        // rd is used to seed the Mersenne Twister
-        std::random_device rd;
-        std::mt19937 generator(rd());
+		// rd is used to seed the Mersenne Twister
+		std::random_device rd;
+		std::mt19937 generator(rd());
 
-        std::uniform_real_distribution<> density_gen(mean_density - variation,
-                                                   mean_density + variation);
-        std::uniform_int_distribution<> coord_gen(0, NM_CHUNK_SIZE - 1);
+		std::uniform_real_distribution<> density_gen(mean_density - variation,
+													 mean_density + variation);
+		std::uniform_int_distribution<> coord_gen(0, NM_CHUNK_SIZE - 1);
 
-        double mine_density = density_gen(generator);
-        int nmines = std::floor(mine_density * (NM_CHUNK_SIZE*NM_CHUNK_SIZE));
+		double mine_density = density_gen(generator);
+		int nmines = std::floor(mine_density * (NM_CHUNK_SIZE*NM_CHUNK_SIZE));
 
-        std::cerr << "[ckgen] generating chunk with " << nmines << " mines"
-                  << " (" << mine_density * 100.0 << "%)" << std::endl;
+		BOOST_LOG_TRIVIAL(info) << "[ckgen] generating chunk with " << nmines << " mines"
+				  << " (" << mine_density * 100.0 << "%)";
 
-        while (nmines > 0) {
-            int rand_x = coord_gen(generator);
-            int rand_y = coord_gen(generator);
+		while (nmines > 0)
+		{
+			int rand_x = coord_gen(generator);
+			int rand_y = coord_gen(generator);
 
-            Square& current_square = chunk.get({rand_x, rand_y});
-            if (!current_square.is_mine) {
-                current_square.is_mine = 1;
-                nmines--;
-            }
-        }
+			Square& current_square = chunk.get({rand_x, rand_y});
+			if (!current_square.is_mine)
+			{
+				current_square.is_mine = 1;
+				nmines--;
+			}
+		}
 
-        return chunk;
-    }
+		return chunk;
+	}
 }
