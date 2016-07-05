e246de0afe628e915fa8d140a596a3721acd230f
Sam van Kampen <sam@tehsvk.net>
2016-07-05 15:50:10 +0200

    I can only describe this as a sort of one-way switch?

---
+++ include/nm/Flag.hpp
@@ -0,0 +1,25 @@
+#ifndef NM_FLAG_HPP
+#define NM_FLAG_HPP
+
+namespace nm
+{
+	class Flag
+	{
+		private:
+			bool flag = false;
+		public:
+			bool operator()()
+			{
+				bool temporary = flag;
+				flag = false;
+				return temporary;
+			}
+
+			void set()
+			{
+				flag = true;
+			}
+	};
+}
+
+#endif
