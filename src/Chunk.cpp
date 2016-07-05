6fdae9b1235f1ea937084130fa2f3642c82098ab
Sam van Kampen <sam@tehsvk.net>
2016-07-05 15:47:28 +0200

    [chunk] Add transform_copy function

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -5,4 +5,13 @@ namespace nm
     Square& Chunk::get(Coordinates coordinates) {
         return chunk[coordinates.y()][coordinates.x()];
     }
-}
+}	Chunk Chunk::transform_copy(std::function<void(Square&)> functor)
+	Chunk Chunk::transform_copy(std::function<void(Square&)> functor)
+	{
+		Chunk temp = *this;
+		std::for_each(temp.chunk.begin(), temp.chunk.end(), [&functor](std::array<Square, NM_CHUNK_SIZE>& row)
+		{
+			std::for_each(row.begin(), row.end(), functor);
+		});
+		return temp;
+	}
