3e190e748cf9a51ffd415a6c28db514ed67651b1
Sam van Kampen <sam@tehsvk.net>
2016-07-06 14:36:49 +0200

    [Game] Make sure the functor only gets called once on a specific chunk
    This is important because otherwise chunks may get sent over the wire
    way too often, resulting in significant slowdown.

--- include/nm/Game.hpp
+++ include/nm/Game.hpp
@@ -4,16 +4,18 @@
 #include "Board.hpp"
 #include <nm/Typedefs.hpp>
 
+#include <unordered_set>
+
 namespace nm
 {
 	class Game
 	{
 		private:
 			Coordinates open_square(int x, int y);
-			bool initialized;
 		public:
 			Board board;
 			void flag_square_handler(int x, int y);
+			std::unordered_set<Coordinates> open_square_recursive(int x, int y);
 			void open_square_handler(int x, int y, std::function<void(int, int, Chunk&)> functor);
 	};
 }
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,4 @@
 #include <nm/Game.hpp>
-#include <vector>
 
 namespace nm
 {
@@ -44,15 +43,9 @@ namespace nm
 		}
 	}
 
-	void Game::open_square_handler(int x, int y, std::function<void (int, int, Chunk&)> functor)
+	std::unordered_set<Coordinates> Game::open_square_recursive(int x, int y)
 	{
-		if (this->initialized == false)
-		{
-			this->initialized = true;
-			board.clear_at(x, y);
-		}
-
-		std::vector<Coordinates> affected_chunks;
+		std::unordered_set<Coordinates> affected_chunks;
 
 		Square &square = board.get(x, y);
 		static int around_offsets[3] = {-1, 0, 1};
@@ -61,20 +54,27 @@ namespace nm
 		{
 			// Do nothing, as flagged squares cannot be opened
 			// and opened squares are already opened
-			return;
+			return {};
 		}
 
-		if (affected_chunks.push_back(open_square(x, y)), square.number == 0)
+		if (affected_chunks.insert(open_square(x, y)), square.number == 0)
 		{
 			for (auto&& xoff : around_offsets)
 			{
 				for (auto&& yoff : around_offsets)
 				{
-					open_square_handler(x+xoff, y+yoff, functor);
+					auto rec_chunks = open_square_recursive(x+xoff, y+yoff);
+					affected_chunks.insert(rec_chunks.begin(), rec_chunks.end());
 				}
 			}
 		}
 
+		return affected_chunks;
+	}
+
+	void Game::open_square_handler(int x, int y, std::function<void (int, int, Chunk&)> functor)
+	{
+		auto affected_chunks = open_square_recursive(x, y);
 		for (auto coordinates : affected_chunks)
 		{
 			Chunk &chunk = board.get_chunk(coordinates);
