cd4fc37fc518dd0614814ae42c39797d4b10a2d8
Sam van Kampen <sam@tehsvk.net>
2017-03-18 16:02:11 +0100

    [view] Start adding view infra

---
+++ include/nm/View.hpp
@@ -0,0 +1,40 @@
+#ifndef _NM_VIEW_
+#define _NM_VIEW_
+
+#include <ncurses.h>
+#include <netmine.pb.h>
+#include <nm/SquareSource.hpp>
+#include <nm/Chunk.hpp>
+#include <nm/Window.hpp>
+#include <nm/CursorData.hpp>
+#include <unordered_map>
+
+namespace nm
+{
+	class View
+	{
+		public:
+			virtual ~View()
+			{
+			};
+
+			virtual void draw_main(Window& main, SquareSource& squareSource,
+					std::unordered_map<int32_t, CursorData> others, CursorData& self) = 0;
+			virtual void draw_sidebar(Window& sidebar, SquareSource& squareSource) {};
+			virtual void handle_input() {};
+
+			virtual void player_quit_handler(const message::Player& player)
+			{
+			};
+
+			virtual void player_join_handler(const message::Player& player)
+			{
+			};
+
+			virtual void cursor_move_handler(const message::CursorMove& msg)
+			{
+			};
+	};
+}
+
+#endif //_NM_VIEW_
