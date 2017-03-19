312cb31f9e5915a89c99c22bf90202e1e172e3ee
Sam van Kampen <sam@tehsvk.net>
2017-03-19 23:58:24 +0100

    [typedefs] Add CursorMap typedef

--- include/nm/Typedefs.hpp
+++ include/nm/Typedefs.hpp
@@ -3,6 +3,8 @@
 
 #include <boost/signals2.hpp>
 #include <simmo/point.hpp>
+#include <nm/CursorData.hpp>
+#include <unordered_map>
 
 const int BORDER_COLOR = 15;
 
@@ -10,6 +12,7 @@ namespace nm
 {
 	using Coordinates  = simmo::point<int32_t, 2>;
 	using SquareEvent  = boost::signals2::signal<void (int, int)>;
+	using CursorMap	   = std::unordered_map<int32_t, CursorData>;
 }
 
 #endif // TYPEDEFS_HPP
