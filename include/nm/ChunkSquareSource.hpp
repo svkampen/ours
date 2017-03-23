b76760bea048d30bc343f400b17cdb8cce6478e9
Sam van Kampen <sam@tehsvk.net>
2017-03-23 20:27:22 +0100

    Add ChunkSquareSource mixin

---
+++ include/nm/ChunkSquareSource.hpp
@@ -0,0 +1,14 @@
+#ifndef NM_CHUNKSQUARESOURCE_HPP
+#define NM_CHUNKSQUARESOURCE_HPP
+
+#include "ChunkSource.hpp"
+#include "SquareSource.hpp"
+
+namespace nm
+{
+	class ChunkSquareSource : public ChunkSource, public SquareSource
+	{
+	};
+}
+
+#endif
