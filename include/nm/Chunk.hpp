31a0ae735b402f329ea694a85f99cda448e6afe5
Sam van Kampen <sam@tehsvk.net>
2017-03-23 20:30:25 +0100

    [Chunk] Add all_of function

--- include/nm/Chunk.hpp
+++ include/nm/Chunk.hpp
@@ -14,12 +14,15 @@ namespace nm
 	class Chunk : public SquareSource
 	{
 		using SquareFn = std::function<void(Square&)>;
+		using ConstSquareFn = std::function<bool(const Square&)>;
 		private:
 			std::array<std::array<Square, NM_CHUNK_SIZE>, NM_CHUNK_SIZE> chunk{}; // {} necessary for 0-init
 		public:
 			static Chunk CHUNK_EMPTY;
 			Square& get(const Coordinates& coordinates);
 			Square& get(int x, int y);
+
+			bool all_squares(const ConstSquareFn& functor) const;
 
 			const Square& get(const Coordinates& coordinates) const;
 			const Square& get(const int x, const int y) const;
